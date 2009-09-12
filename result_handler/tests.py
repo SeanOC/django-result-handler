c559fc0758a2e49a729141da6b0bee6c22c906a3
Sean O'Connor <sean.b.oconnor@gmail.com>
2009-09-12 13:16:31 -0700

    More unit tests.

--- result_handler/tests.py
+++ result_handler/tests.py
@@ -17,6 +17,9 @@ class Book(models.Model):
     
 class Coffee(models.Model):
     brand = models.CharField(max_length=255, db_column="name")
+    
+class Reviewer(models.Model):
+    reviewed = models.ManyToManyField(Book)
 
 
 class ResultHandlerTests(TestCase):
@@ -25,6 +28,7 @@ class ResultHandlerTests(TestCase):
         self.authors = []
         self.books = []
         self.coffees = []
+        self.reviewers = []
         
         self.authors.append(Author.objects.create(
             first_name="Joe",
@@ -70,25 +74,40 @@ class ResultHandlerTests(TestCase):
         self.coffees.append(Coffee.objects.create(brand="dunkin doughnuts"))
         self.coffees.append(Coffee.objects.create(brand="starbucks"))
         
+        self.reviewers.append(Reviewer.objects.create())
+        self.reviewers.append(Reviewer.objects.create())
+        self.reviewers[0].reviewed.add(self.books[3])
+        self.reviewers[0].reviewed.add(self.books[1])
+        self.reviewers[0].reviewed.add(self.books[2])
+        
     def assertHandled(self, handled, orig):
         self.assertEqual(len(handled), len(orig))
         for index, item in enumerate(handled):
             self.assertEqual(item, orig[index])
+            
+    def assertNoAnnotations(self, handled):
+        self.assertAnnotations(handled, ())
+        
+    def assertAnnotations(self, handled, expected_annotations):
+        self.assertEqual(handled._annotations, expected_annotations)
         
     def testSimpleHandler(self):
         query = "SELECT * FROM result_handler_author"
         handled_authors = ResultHandler(Author, query)
         self.assertHandled(handled_authors, self.authors)
+        self.assertNoAnnotations(handled_authors)
         
     def testFkeyHandler(self):
         query = "SELECT * FROM result_handler_book"
         handled_books = ResultHandler(Book, query)
         self.assertHandled(handled_books, self.books)
+        self.assertNoAnnotations(handled_books)
         
     def testDBColumnHandler(self):
         query = "SELECT * FROM result_handler_coffee"
         handled_coffees = ResultHandler(Coffee, query)
         self.assertHandled(handled_coffees, self.coffees)
+        self.assertNoAnnotations(handled_coffees)
         
     def testOrderHandler(self):
         selects = (
@@ -101,6 +120,7 @@ class ResultHandlerTests(TestCase):
             query = "SELECT %s FROM result_handler_author" % select
             handled_authors = ResultHandler(Author, query)
             self.assertHandled(handled_authors, self.authors)
+            self.assertNoAnnotations(handled_authors)
             
     def testTranslations(self):
         query = "SELECT first_name AS first, last_name AS last, dob, id FROM result_handler_author"
@@ -109,6 +129,7 @@ class ResultHandlerTests(TestCase):
             ('last', 'last_name'),
         )
         handled_authors = ResultHandler(Author, query, translations=translations)
+        self.assertNoAnnotations(handled_authors)
         
     def testParams(self):
         query = "SELECT * FROM result_handler_author WHERE first_name = %s"
@@ -116,3 +137,8 @@ class ResultHandlerTests(TestCase):
         handled_authors = ResultHandler(Author, query, params=params)
         self.assertEqual(len(handled_authors), 1)
         self.assertHandled(handled_authors, [self.authors[2]])
+        
+    def testManyToMany(self):
+        query = "SELECT * FROM result_handler_reviewer"
+        handled_reviewers = ResultHandler(Reviewer, query)
+        self.assertHandled(handled_reviewers, self.reviewers)
